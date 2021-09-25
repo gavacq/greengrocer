2f75a6513642cdcc9b5e246eebcc8658c30ecb4d
solenedel <solene98@student.ubc.ca>
2021-09-24 17:34:54 -0700

    add users seeds

---
+++ db/seeds/01_users.sql
@@ -0,0 +1,15 @@
+
+
+-- Seeds for users table
+
+INSERT INTO users (id, username, email, password, date_joined)
+VALUES (1, 'Geecrypt', 'geecrypt@mail.com', '6253efdgy');
+
+INSERT INTO users (id, username, email, password, date_joined)
+VALUES (2, 'Mario', 'mario@mail.com', '87ywjh');
+
+INSERT INTO users (id, username, email, password, date_joined)
+VALUES (3, 'Nally', 'nally@mail.com', 'qeufhew');
+
+
+-- ALTER SEQUENCE users_id_seq RESTART WITH 5;

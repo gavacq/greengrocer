cc4c9682a9f0fbdffd3edf2805f767260f03080b
geecrypt <gavina352@gmail.com>
2021-09-25 08:32:51 -0700

    connect backend to database and render user data in react

--- server/server.js
+++ server/server.js
@@ -3,10 +3,26 @@ const path = require("path");
 const App = Express();
 const PORT = 8081;
 
-// Postgres config
-const PORT = process.env.PORT || 8080;
-const ENV = process.env.ENV || "development";
-const express = require("express");
+// PG database client/connection setup
+require('dotenv').config();
+const { Pool } = require('pg');
+let dbParams = {};
+if (process.env.DATABASE_URL) {
+  dbParams.connectionString = process.env.DATABASE_URL;
+} else {
+  dbParams = {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+  };
+}
+
+console.log('dbParams', dbParams);
+
+const db = new Pool(dbParams);
+db.connect()
 
 // Express Configuration
 App.use(Express.urlencoded({ extended: false }));
@@ -15,11 +31,15 @@ App.use(Express.static(path.join(__dirname, "..", "build")));
 App.use(Express.static("public"));
 
 // Sample GET route
-App.get("/api/data", (req, res) =>
-  res.json({
-    message: "Seems to work!"
-  })
-);
+App.get("/api/data", (req, res) => {
+  const queryString = "SELECT * FROM users"
+  db.query(queryString)
+  .then((data) => {
+    return res.json({
+    message: "Seems to work!",
+    data: data.rows
+  })})
+});
 
 App.listen(PORT, () => {
   // eslint-disable-next-line no-console
--- src/App.js
+++ src/App.js
@@ -5,20 +5,22 @@ import './App.css';
 
 function App() {
   const [helloWorld, sethelloWorld] = useState('')
+  const [users, setUsers] = useState({})
 
   useEffect(() => {
     axios.get('api/data')
       .then((res) => {
-
         sethelloWorld(res.data.message)
+        setUsers(res.data.data)
       });
-    
   }, []);
 
+  console.log(users)
   return (
     <div className="App">
       <p>{helloWorld}</p>
       <p>groceryboiz !</p>
+      <p>{users[0] && users[0].username}</p>
     </div>
   );
 }

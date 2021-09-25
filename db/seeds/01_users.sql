98db31d91f6cbf37098f5ff468a60019339bd2e0
solenedel <solene98@student.ubc.ca>
2021-09-25 11:39:29 -0700

    fix suyntax errors in seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -12,40 +12,38 @@ INSERT INTO users (username, email, password)
 VALUES ('Nally', 'nally@mail.com', 'qeufhew');
 
 
--- ALTER SEQUENCE users_id_seq RESTART WITH 4;
-
-
 -- Seeds for lists table
 
-INSERT INTO lists (co2_data)
-VALUES (30)
+INSERT INTO lists (user_id, co2_data)
+VALUES (1, 30);
 
-INSERT INTO lists (co2_data)
-VALUES (100)
+INSERT INTO lists (user_id, co2_data)
+VALUES (2, 100);
 
-INSERT INTO lists (co2_data)
-VALUES (68)
-
+INSERT INTO lists (user_id, co2_data)
+VALUES (3, 68);
 
 -- Seeds for posts table
 
-INSERT INTO posts (likes, message)
-VALUES (5, 'you saved lots of CO2')
+INSERT INTO posts (user_id, likes, message)
+VALUES (1, 5, 'you saved lots of CO2');
 
-INSERT INTO posts (likes, message)
-VALUES (2, 'lorem ipsum and some other stuff')
+INSERT INTO posts (user_id, likes, message)
+VALUES (2, 2, 'lorem ipsum and some other stuff');
 
-INSERT INTO posts (likes, message)
-VALUES (23, 'very interesting message')
+INSERT INTO posts (user_id, likes, message)
+VALUES (3, 23, 'very interesting message');
 
 
 -- Seeds for products table
 
 INSERT INTO products (lat, long, co2_data)
-VALUES (49.1, 23.5, 103)
+VALUES (49.1, 23.5, 103);
 
 INSERT INTO products (lat, long, co2_data)
-VALUES (0, 0, 300)
+VALUES (0, 0, 300);
 
 INSERT INTO products (lat, long, co2_data)
-VALUES (15, 30, 609)
+VALUES (15, 30, 609);
+
+

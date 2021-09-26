734a73eb80e3f74bf5770bcbeed8284eed92a695
Lliam McBean <lliam.mcbean@gmail.com>
2021-09-26 11:28:19 -0600

    started click handler logic

--- src/components/Search/SearchBar.jsx
+++ src/components/Search/SearchBar.jsx
@@ -1,9 +1,18 @@
+import axios from 'axios';
 import { React, useState } from 'react';
 
 export default function SearchBar() {
   const [productName, setProductName] = useState('');
+  // helper: handles click on the search button
+  const clickHandler = () => {
+    console.log('inside clickHandler');
+    // https://api.spoonacular.com/food/products/22347?apiKey=f8dd3554d0444ddba0bdd8e8237f980d&includeNutrition=true
+    axios.get('https://api.spoonacular.com/food/products/22347?apiKey=f8dd3554d0444ddba0bdd8e8237f980d&includeNutrition=true')
+      .then((res) => console.log(res.data));
+  };
+
   return (
-    <form>
+    <form onSubmit={(event) => event.preventDefault()}>
       <input
         placeholder="search for a product"
         product="product"
@@ -11,7 +20,7 @@ export default function SearchBar() {
         value={productName}
         onChange={(event) => setProductName(event.target.value)}
       />
-      <button type="submit">Search</button>
+      <button type="button" onClick={clickHandler}>Search</button>
     </form>
   );
 }

ef7c3f2c3e61fb9d7cf86c761148137768afb22a
geecrypt <gavina352@gmail.com>
2021-09-27 11:58:01 -0700

    add axios request to login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import { React, useState } from 'react';
+import axios from 'axios';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleClick = () => {
+    axios.post('/login', { email, password });
+  };
+
   return (
     <main>
       <h1>
         Login
       </h1>
+      <section>
+        <form onSubmit={handleSubmit}>
+          <p>email</p>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <p>password</p>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <br />
+          <button type="button" onClick={handleClick}>Login</button>
+        </form>
+      </section>
     </main>
   );
 }

a05af77ec7f4d4d581a68a9012781b133f991a01
solenedel <solene98@student.ubc.ca>
2021-09-27 14:17:52 -0700

    move styles from App to Home component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,10 +24,6 @@ function App() {
             <Route path="/lists" component={Lists} />
             <Route path="/stats" component={Stats} />
           </Switch>
-
-          <div className="icon strawberry">
-            <img src="images/strawberry.png" alt="strawberry icon" />
-          </div>
         </section>
 
         <section className="green">
@@ -36,12 +32,6 @@ function App() {
               <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className="shape-fill" />
             </svg>
           </div>
-
-          <section className="home-button">
-            <div className="btn-wrapper">
-              <button type="button" className="home-btn">create new list</button>
-            </div>
-          </section>
         </section>
       </div>
     </Router>
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <h1>
-      Welcome to GreenGrocer.
-    </h1>
+    <section className="home">
+      <h1>
+        Welcome to GreenGrocer.
+      </h1>
+      <div className="icon strawberry">
+        <img src="images/strawberry.png" alt="strawberry icon" />
+      </div>
+      <div className="btn-wrapper">
+        <button type="button" className="home-btn">create new list</button>
+      </div>
+    </section>
   );
 }

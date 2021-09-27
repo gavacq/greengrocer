cb47018803bcb354fb3fd4a9c738a7f286e00ce1
geecrypt <gavina352@gmail.com>
2021-09-26 17:41:52 -0700

    rename ListProducts components to NewList

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,14 +1,13 @@
 import { React, useState } from 'react';
-// import PropTypes from 'prop-types';
 import Search from './Search';
-import ListProducts from './ListProducts';
+import NewList from './NewList';
 
 export default function List() {
   const [newProduct, setNewProduct] = useState({});
   return (
     <main>
       <Search setNewProduct={setNewProduct} />
-      <ListProducts newProduct={newProduct} />
+      <NewList newProduct={newProduct} />
     </main>
   );
 }

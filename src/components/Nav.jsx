ca5c2d48e9f1fb5401f56d812e6e1ced21585495
geecrypt <gavina352@gmail.com>
2021-09-26 18:44:13 -0700

    add nav style

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import '../App.css';
+import { Link } from 'react-router-dom';
 
 export default function Nav() {
   return (
-    <h1>
-      Nav
-    </h1>
+    <nav>
+      <Link to="/">
+        <h3>Logo</h3>
+      </Link>
+      <ul className="nav-links">
+        <Link to="/lists">
+          <li>Lists</li>
+        </Link>
+        <Link to="/stats">
+          <li>Stats</li>
+        </Link>
+      </ul>
+    </nav>
   );
 }

393584f1dc54984f5eb477f6f0142e6e652e2e63
geecrypt <gavina352@gmail.com>
2021-09-28 15:02:20 -0700

    rename list.js to lists

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,7 @@
 const searchRoute = require('./search');
 const loginRoute = require('./login');
 const logoutRoute = require('./logout');
-const listRoute = require('./list');
+const listRoute = require('./lists');
 
 module.exports = {
   searchRoute,

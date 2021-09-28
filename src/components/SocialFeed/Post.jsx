346ffef20eacb7aaeddbdf0969f227669fa9e696
geecrypt <gavina352@gmail.com>
2021-09-28 16:18:13 -0700

    prelim like button styling

--- src/components/SocialFeed/Post.jsx
+++ src/components/SocialFeed/Post.jsx
@@ -5,11 +5,29 @@ export default function Post(props) {
   const {
     id, username, likes, message,
   } = props;
+
+  const heartButtonStyle = {
+    cursor: 'pointer',
+    backgroundColor: 'rgba(1,1,1,0)',
+    border: 'none',
+  };
+
+  const heartStyle = {
+    width: '20px',
+  };
+
+  const likePost = () => {
+
+  };
+
   return (
     <article data-post-id={id}>
       <h1>Post</h1>
       <p>{username}</p>
-      <p>{likes}</p>
+      <button type="button" style={heartButtonStyle} onClick={likePost}>
+        <img src="images/heart.png" style={heartStyle} alt="like" />
+      </button>
+      {likes}
       <p>{message}</p>
     </article>
   );
@@ -19,5 +37,5 @@ Post.propTypes = {
   id: PropTypes.number.isRequired,
   username: PropTypes.string.isRequired,
   likes: PropTypes.number.isRequired,
-  message: PropTypes.number.isRequired,
+  message: PropTypes.string.isRequired,
 };

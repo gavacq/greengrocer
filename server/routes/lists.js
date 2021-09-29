3099238a51baae588202ea39a0523b2e9133fff3
geecrypt <gavina352@gmail.com>
2021-09-29 14:44:58 -0700

    get product id for duplicate products on insert conflict

--- server/routes/lists.js
+++ server/routes/lists.js
@@ -3,13 +3,17 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = (db) => {
-  router.put('/', (req) => {
+  router.put('/', (req, res) => {
     const products = req.body.newList;
+    if (!products.length) {
+      res.send('empty products list');
+      return;
+    }
     console.log('PUT /api/lists', req.body);
 
     const productsValuesSql = products.map((p) => `(${p.api_id}, '${p.title}', '${p.image}', ${p.lat}, ${p.long}, ${p.co2})`);
     // TODO: fix SQL injection vulnerabilities
-    const productsSql = `INSERT INTO products(api_product_id, title, image, lat, long, co2_data) VALUES ${productsValuesSql} ON CONFLICT (api_product_id) DO NOTHING RETURNING id`;
+    const productsSql = `INSERT INTO products(api_product_id, title, image, lat, long, co2_data) VALUES ${productsValuesSql} ON CONFLICT (api_product_id) DO UPDATE SET api_product_id=EXCLUDED.api_product_id RETURNING id`;
     console.log(productsSql);
     // insert multiple products
     // check api_id column for conflicts, if so do nothing
@@ -39,23 +43,20 @@ module.exports = (db) => {
     Promise.all([productsPromise, listsPromise])
       .then((data) => {
         console.log('This is promise data: ', data);
+        if (!data[0].length) {
+          console.log('no products to insert');
+
+          return Promise.resolve();
+        }
         const productsListsValuesSql = data[0].map((p) => `(${p.id}, ${data[1][0].id})`);
         console.log('sql', productsListsValuesSql);
-
         return db.query(`INSERT INTO products_lists(product_id, list_id) VALUES ${productsListsValuesSql}`);
       })
-      .then(() => console.log('successfully insert products_lists'))
+      .then(() => {
+        console.log('successfully insert products_lists');
+        res.send('saved');
+      })
       .catch((error) => console.log('products_lists insert failed', error));
-
-    // -- lists
-    // user_id -> req.session
-    // total c02 saved for list -> TODO
-    // -- products
-    // c02 for product -> y
-    // lat, long -> y
-    // title -> y
-    // image -> y
-    // api product id -> y
   });
   return router;
 };

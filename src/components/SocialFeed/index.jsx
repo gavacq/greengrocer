814fd52c90abe8124b411f82efda7ac243400e3d
solenedel <solene98@student.ubc.ca>
2021-09-30 11:46:39 -0700

    style social feed

--- src/components/SocialFeed/index.jsx
+++ src/components/SocialFeed/index.jsx
@@ -21,7 +21,7 @@ export default function SocialFeed() {
           <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className="shape-fill" />
         </svg>
       </div>
-      <h1>Social feed</h1>
+      <h1 className="social-title">Social feed</h1>
       {posts.map((post) => (
         <Post
           key={post.id}

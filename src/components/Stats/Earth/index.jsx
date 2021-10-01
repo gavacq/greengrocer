65f73e6c988d90c4e120425ec5dcfb65243845d1
Lliam McBean <lliam.mcbean@gmail.com>
2021-10-01 11:33:23 -0600

    added bezier functionality

--- src/components/Stats/Earth/index.jsx
+++ src/components/Stats/Earth/index.jsx
@@ -5,12 +5,14 @@ import { OrbitControls } from '@react-three/drei';
 import Planet from './Planet';
 import Clouds from './Clouds';
 import Location from './Location';
+import Bezier from './Bezier';
 
 export default function Earth() {
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
+    <div style={{ width: '100vw', height: '90vh' }}>
       <Canvas>
         <ambientLight intensity={0.8} />
+        <Bezier />
         <Suspense fallback={null}>
           <Planet />
           <Clouds />

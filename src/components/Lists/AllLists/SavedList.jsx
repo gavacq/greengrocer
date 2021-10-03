314bd3c6ce68e3cf230f255318213522c5d87206
solenedel <solene98@student.ubc.ca>
2021-10-03 09:54:12 -0700

    work on generateMessage function

--- src/components/Lists/AllLists/SavedList.jsx
+++ src/components/Lists/AllLists/SavedList.jsx
@@ -23,19 +23,19 @@ export default function SavedList(props) {
     switch (Math.floor((Math.random() * 100) % 5)) {
       case 0:
         // lightbulb
-        return `I saved ${co2}kg of CO2. That's like having ${1} incandescent lightbulb on for ${1} hours!`;
+        return `I saved ${co2} kg of CO2. That's like having ${1} incandescent lightbulb on for ${1} hours!`;
       case 1:
         // airplane
-        return `I saved ${co2}kg of CO2. That's like flying for ${1} hours!`;
+        return `I saved ${co2} kg of CO2. That's like flying in economy class for ${(co2 / 110)} hours!`;
       case 2:
         // car
-        return `I saved ${co2}kg of CO2. That's like driving for ${1} hours`;
+        return `I saved ${co2} kg of CO2. That's like driving ${(co2 / 0.28)} km in a typical passenger vehicle!`;
       case 3:
         // hours of watching netflix
-        return `I saved ${co2}kg of CO2. That's like ${1} hours of watching netflix!`;
+        return `I saved ${co2} kg of CO2. That's like ${1} hours of watching netflix!`;
       case 4:
         // trees equivalent
-        return `I saved ${co2}kg of CO2. An average tree takes ${1} hours to remove that much CO2 from the atmosphere!`;
+        return `I saved ${co2} kg of CO2. An average tree takes ${1} hours to remove that much CO2 from the atmosphere!`;
       default:
         return 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia nam commodi aliquid ipsum, unde nemo explicabo animi molestiae voluptas, omnis tempore aliquam corporis quas optio fugit asperiores laboriosam maiores. Ab?';
     }

b6b51ee7a30266921686cf6e66de894ea5f30de4
geecrypt <gavina352@gmail.com>
2021-10-04 13:50:01 -0700

    parameterize posts queries

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -21,13 +21,15 @@ module.exports = (db, io) => {
   };
 
   const queryPost = (id) => {
-    const postQuery = `SELECT (posts.*), (users.username) FROM posts JOIN users ON users.id = user_id WHERE posts.id = ${id}`;
-    return db.query(postQuery);
+    const postSql = 'SELECT (posts.*), (users.username) FROM posts JOIN users ON users.id = user_id WHERE posts.id = $1';
+    const postValue = [id];
+    return db.query(postSql, postValue);
   };
 
   const queryLikedPosts = (user) => {
-    const likedPostsQuery = `SELECT posts.id FROM posts JOIN liked_posts ON liked_posts.post_id = posts.id WHERE liked_posts.user_id = ${user}`;
-    return db.query(likedPostsQuery);
+    const likedPostsSql = 'SELECT posts.id FROM posts JOIN liked_posts ON liked_posts.post_id = posts.id WHERE liked_posts.user_id = $1';
+    const likedPostsValue = [user];
+    return db.query(likedPostsSql, likedPostsValue);
   };
 
   const addLikedFlagToPosts = (ogPosts, likedPosts) => ogPosts.map((p) => ({
@@ -68,17 +70,19 @@ module.exports = (db, io) => {
   // update likes for a post
   router.patch('/:postId', (req, res) => {
     const updateLikes = (likedByUser, postId) => {
-      let likedPostsQuery = '';
-      let postsQuery = '';
+      let likedPostsSql = '';
+      let postsSql = '';
       if (likedByUser) {
-        likedPostsQuery = `INSERT INTO liked_posts (user_id, post_id) VALUES (${req.session.user}, ${postId})`;
-        postsQuery = `UPDATE posts SET likes = likes + 1 WHERE posts.id = ${postId} RETURNING likes`;
+        likedPostsSql = 'INSERT INTO liked_posts (user_id, post_id) VALUES ($1, $2)';
+        postsSql = 'UPDATE posts SET likes = likes + 1 WHERE posts.id = $1 RETURNING likes';
       } else {
-        likedPostsQuery = `DELETE FROM liked_posts WHERE user_id = ${req.session.user} AND post_id = ${postId}`;
-        postsQuery = `UPDATE posts SET likes = likes - 1 WHERE posts.id = ${postId} RETURNING likes`;
+        likedPostsSql = 'DELETE FROM liked_posts WHERE user_id = $1 AND post_id = $2';
+        postsSql = 'UPDATE posts SET likes = likes - 1 WHERE posts.id = $1 RETURNING likes';
       }
-      const likedPostsPromise = db.query(likedPostsQuery);
-      const postsPromise = db.query(postsQuery);
+      const likedPostsValues = [req.session.user, postId];
+      const postsValue = [postId];
+      const likedPostsPromise = db.query(likedPostsSql, likedPostsValues);
+      const postsPromise = db.query(postsSql, postsValue);
 
       // eslint-disable-next-line max-len
       return Promise.all([likedPostsPromise, postsPromise]).then((data) => ({ likedByUser, likes: data[1].rows[0].likes }));
@@ -110,8 +114,9 @@ module.exports = (db, io) => {
       return;
     }
 
-    const sql = `INSERT INTO posts(user_id, likes, message) VALUES(${req.session.user}, 0, '${post.message}') RETURNING id`;
-    db.query(sql)
+    const sql = 'INSERT INTO posts(user_id, likes, message) VALUES($1, 0, $2) RETURNING id';
+    const values = [req.session.user, post.message];
+    db.query(sql, values)
       .then((data) => {
         console.log('posts insert res', data.rows[0]);
         return queryPost(data.rows[0].id);
@@ -121,7 +126,10 @@ module.exports = (db, io) => {
 
         res.json(data.rows[0]);
       })
-      .catch((e) => console.log('error:', e));
+      .catch((e) => {
+        console.log('error:', e);
+        res.json({});
+      });
   });
 
   return router;

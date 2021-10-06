114a5a903baa71ee171c70ee7c2b4f995a4c5ee4
solenedel <solene98@student.ubc.ca>
2021-10-05 19:37:01 -0700

    minor style edits

--- src/components/SocialFeed/index.jsx
+++ src/components/SocialFeed/index.jsx
@@ -48,13 +48,15 @@ export default function SocialFeed() {
 
   return (
     <section className="social-section">
-      <img className="social-icon" src="./images/love.png" alt="social" />
       <div className="wave">
         <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
           <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className="shape-fill" />
         </svg>
       </div>
-      <h1 className="social-title">Social feed</h1>
+      <div className="title-icon">
+        <h1 className="social-title"> Social feed </h1>
+        <img className="social-icon" src="./images/love.png" alt="social" />
+      </div>
       <div className="temp">
         <div className="posts-wrapper">
           {posts.map((post) => (

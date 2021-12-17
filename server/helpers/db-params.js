c0f02b88e0f4ff631f30d4879eb53f5523fa28a7
geecrypt <gavina352@gmail.com>
2021-12-17 00:28:44 -0800

    Change dbparams

--- server/helpers/db-params.js
+++ server/helpers/db-params.js
@@ -1,16 +1,14 @@
 require('dotenv').config();
 
 let dbParams = {};
-if (process.env.DATABASE_URL) {
-  dbParams.connectionString = process.env.DATABASE_URL;
-} else {
-  dbParams = {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-  };
-}
+console.log('env = ', process.env)
+dbParams = {
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  connectionString: process.env.DATABASE_URL
+};
 
 module.exports = dbParams;
